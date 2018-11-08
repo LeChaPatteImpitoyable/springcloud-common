fab0565f8e555ffbb5e90192afa6fb77d2f4732e
yingsy <yingshengyu@outlook.com>
2018-11-08 20:55:21 +0800

    init springcloud common

---
+++ src/main/java/com/yasaka/stock/constants/ProviderUrlConstant.java
@@ -0,0 +1,14 @@
+package com.yasaka.stock.constants;
+
+/**
+ * Created by yingsy on 2018/11/8.
+ */
+public class ProviderUrlConstant {
+
+    private ProviderUrlConstant() {
+    }
+
+    public static final String USER_MAPPING = "user";
+    public static final String USER_GETUSER = "{id}";
+    public static final String USER_LIST = "list";
+}
